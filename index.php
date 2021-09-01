7ede875d96385ee8b149512dce0f549ab6719322
MattiaR <rovitomattia91@gmail.com>
2021-09-01 19:13:59 +0200

    stampato a schermo i guadagni al botteghino

--- index.php
+++ index.php
@@ -38,6 +38,19 @@ $film1->setGuadagno('2.798.000.000');
 var_dump($film1);
 
 
+//? Stampo a schermo il guadagno al botteghino
+$avengers = $film1->getGuadagno();
+echo "Il film Avengers: End Game ha guadagnato " . $avengers . " di dollari al botteghino";
+
+
+
+
+
 $film2 = new Movie('Spider-Man: Homecoming', 2017);
 $film2->setGuadagno('880.200.000');
 var_dump($film2);
+
+
+//? Stampo a schermo il guadagno al botteghino
+$spider = $film2->getGuadagno();
+echo "Il film Spider-Man: Homecoming ha guadagnato " . $spider . " di dollari al botteghino";
